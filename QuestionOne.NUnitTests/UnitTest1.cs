3e6dc4755552eae203bcb3f954085cd83f912084
Cameron Quayle <robertquaylebusiness@gmail.com>
2022-04-04 04:26:47 +0100

    Initial Commit

---
+++ QuestionOne.NUnitTests/UnitTest1.cs
@@ -0,0 +1,91 @@
+﻿using NUnit.Framework;
+using System;
+using DerivcoUtil;
+
+namespace QuestionOne.NUnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        { }
+
+        [Test]
+        public void Test1()
+        {
+            // String for testing
+            string test_string = "This is a test string";
+
+            // Execute = Encode & Decode
+            string encoded = DerivcoUtil.Encoding.Encode(test_string);
+            string decoded = DerivcoUtil.Encoding.Decode(encoded);
+
+            // Assert
+            string result = (String.Equals(test_string, decoded)) ? "Pass" : "Fail";
+            Assert.Pass(result, "Pass");
+        }
+
+        [Test]
+        public void Test2()
+        {
+            // String for testing
+            string test_string = "This is another test string.";
+
+            // Execute = Encode & Decode
+            string encoded = DerivcoUtil.Encoding.Encode(test_string);
+            string decoded = DerivcoUtil.Encoding.Decode(encoded);
+
+            // Assert
+            string result = (String.Equals(test_string, decoded)) ? "Pass" : "Fail";
+            Assert.Pass(result, "Pass");
+        }
+
+        [Test]
+        public void Test3()
+        {
+            // String for testing
+            string test_string = "Cameron is very handsome.";
+
+            // Execute = Encode & Decode
+            string encoded = DerivcoUtil.Encoding.Encode(test_string);
+            string decoded = DerivcoUtil.Encoding.Decode(encoded);
+
+            // Assert
+            string result = (String.Equals(test_string, decoded)) ? "Pass" : "Fail";
+            Assert.Pass(result, "Pass");
+        }
+
+        [Test]
+        public void Test4()
+        {
+            // String for testing
+            string test_string = "Cameron would be a great addition to MIGS, we should hire him.";
+
+            // Execute = Encode & Decode
+            string encoded = DerivcoUtil.Encoding.Encode(test_string);
+            string decoded = DerivcoUtil.Encoding.Decode(encoded);
+
+            // Assert
+            string result = (String.Equals(test_string, decoded)) ? "Pass" : "Fail";
+            Assert.Pass(result, "Pass");
+        }
+
+
+        [Test]
+        public void Test5()
+        {
+            // String for testing
+            string test_string = "This is Jibberish: fak3hgrlie239232;';'3'4;hefoig 3oioirwoiiuaw39r28yt[";
+
+            // Execute = Encode & Decode
+            string encoded = DerivcoUtil.Encoding.Encode(test_string);
+            string decoded = DerivcoUtil.Encoding.Decode(encoded);
+
+            // Assert
+            string result = (String.Equals(test_string, decoded)) ? "Pass" : "Fail";
+            Assert.Pass(result, "Pass");
+        }
+
+
+    }
+}
---
+++ QuestionOne/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace QuestionOne
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+            string test_string = "This is a test string";
+
+            string encoded = DerivcoUtil.Encoding.Encode(test_string);
+            string decoded = DerivcoUtil.Encoding.Decode(encoded);
+
+            if (String.Equals(test_string, decoded))
+            {
+                Console.WriteLine("Test succeeded");
+            }
+            else
+            {
+                Console.WriteLine("Test failed");
+            }
+
+        }
+
+    }
+}
