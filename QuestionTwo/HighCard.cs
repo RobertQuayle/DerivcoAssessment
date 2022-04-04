25bdcf481282a0a8389aca025e7b30bf6d4dae80
Cameron Quayle <robertquaylebusiness@gmail.com>
2022-04-04 23:00:53 +0100

    Question Two Commit

---
+++ QuestionTwo/HighCard.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace DerivcoGames
+{
+    /*
+     HighCard is an instantiable class for a card game.
+
+     The card game puts player one and two against each other and the player with the
+     highest card value/set wins.
+
+     This class is highly customisable via the Constructor and optional params.
+     */
+
+    public class HighCard
+    {
+        // Fields
+        private string[] range = { "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
+        private string[] sets = { "Clubs", "Diamonds", "Hearts", "Spades" };
+        private bool tieCompareSets = false;
+        private bool noTie = false;
+        private bool wildcard = false;
+
+        // Constructor(s)
+        public HighCard(string[] customRange = null, string[] customSets = null, bool compareTie = false, bool noTie = false, bool wildcard = true)
+        {
+            if (customRange != null)
+            {
+                this.range = customRange;
+            }
+
+            if (customSets != null)
+            {
+                this.sets = customSets;
+            }
+
+            this.tieCompareSets = compareTie;
+
+            this.noTie = noTie;
+
+            this.wildcard = wildcard;
+        }
+
+        // Public Methods
+        public void Play()
+        {
+            Random rnd = new();
+
+            int wildcardValue = 0;
+            int wildcardSet = 0;
+
+            if (wildcard == true)
+            {
+                wildcardValue = rnd.Next(range.Length);
+                wildcardSet = rnd.Next(sets.Length);
+            }
+
+            int playerOneCard = rnd.Next((range.Length));
+            int playerOneSet = rnd.Next((sets.Length));
+            Console.WriteLine($"Player 1: {range[playerOneCard]} of {sets[playerOneSet]}");
+
+            int playerTwoCard = rnd.Next((range.Length));
+            int playerTwoSet = rnd.Next((sets.Length));
+            if (playerTwoCard == playerOneCard)
+            {
+                while (playerTwoSet == playerOneSet)
+                {
+                    playerTwoSet = rnd.Next(sets.Length);
+                }
+            }
+            Console.WriteLine($"Player 2: {range[playerTwoCard]} of {sets[playerTwoSet]}");
+
+            if (wildcard == true && playerOneCard == wildcardValue && playerOneSet == wildcardSet)
+            {
+                Console.WriteLine("Player One Wins by Wildcard!!!");
+            }
+            else if (wildcard == true && playerTwoCard == wildcardValue && playerTwoSet == wildcardSet)
+            {
+                Console.WriteLine("Player Two Wins by Wildcard!!!");
+            }
+            else if (tieCompareSets == true && playerOneCard == playerTwoCard)
+            {
+                if (playerOneSet > playerTwoSet)
+                {
+                    Console.WriteLine("Player One Wins!");
+                }
+                else if (playerOneSet < playerTwoSet)
+                {
+                    Console.WriteLine("Player Two Wins!");
+                }
+            }
+            else if (playerOneCard > playerTwoCard)
+            {
+                Console.WriteLine("Player One Wins!");
+            }
+            else if (playerTwoCard > playerOneCard)
+            {
+                Console.WriteLine("Player Two Wins!");
+            }
+            else
+            {
+                if (noTie == true)
+                {
+                    Console.WriteLine("Re-Draw!");
+                    Play();
+                }
+                else
+                {
+                    Console.WriteLine("It's a Tie!");
+                }
+            }
+
+        }
+
+    }
+
+}
---
+++ QuestionTwo/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using DerivcoGames;
+
+namespace QuestionTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Setup
+            string[] testRange = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "14", "15", "16", "17", "18", "19", "20", };
+
+            string[] testSets = { "Clubs", "Diamonds", "Hearts", "Spades", "Manxs" };
+
+            // Test 1: No Params
+            HighCard testOne = new();
+
+            Console.WriteLine("\n*** TEST ONE ***\n");
+
+            for (int i = 0; i < 10; i++)
+            {
+                testOne.Play();
+                Console.WriteLine("");
+            }
+
+            // Test 2: Custom Range Param
+            HighCard testTwo = new(testRange);
+
+            Console.WriteLine("\n*** TEST TWO ***\n");
+
+            for (int i = 0; i < 10; i++)
+            {
+                testTwo.Play();
+                Console.WriteLine("");
+            }
+
+            // Test 3: Custom Sets Param
+            HighCard testThree = new(null, testSets);
+
+            Console.WriteLine("\n*** TEST THREE ***\n");
+
+            for (int i = 0; i < 10; i++)
+            {
+                testThree.Play();
+                Console.WriteLine("");
+            }
+
+            // Test 4: Custom Range & Sets Params
+            HighCard testFour = new(testRange, testSets);
+
+            Console.WriteLine("\n*** TEST FOUR ***\n");
+
+            for (int i = 0; i < 10; i++)
+            {
+                testFour.Play();
+                Console.WriteLine("");
+            }
+
+            // Test 5: Custom Range, Custom Sets & Compare sets on tie Params
+            HighCard testFive = new(testRange, testSets, true);
+
+            Console.WriteLine("\n*** TEST FIVE ***\n");
+
+            for (int i = 0; i < 10; i++)
+            {
+                testFive.Play();
+                Console.WriteLine("");
+            }
+
+            // Test 6: Custom Range, Custom Sets, Compare sets on tie & No-Tie Params
+            HighCard testSix = new(testRange, testSets, true, true);
+
+            Console.WriteLine("\n*** TEST SIX ***\n");
+
+            for (int i = 0; i < 10; i++)
+            {
+                testSix.Play();
+                Console.WriteLine("");
+            }
+
+            // Test 7: Custom Range, Custom Sets, Compare sets on tie, No-Tie & Wildcard Params
+            HighCard testSeven = new(testRange, testSets, true, true, true);
+
+            Console.WriteLine("\n*** TEST SEVEN ***\n");
+
+            for (int i = 0; i < 10; i++)
+            {
+                testSeven.Play();
+                Console.WriteLine("");
+            }
+
+        }
+    }
+
+}
